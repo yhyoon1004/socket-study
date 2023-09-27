f00210618836407ef32b427cd68f46d1926b09aa
yhyoon1004 <yhyoon1004@naver.com>
2023-09-27 09:58:12 +0900

    간단한 웹소켓 구현

---
+++ src/main/java/com/example/socket4/Socket4Application.java
@@ -0,0 +1,13 @@
+package com.example.socket4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Socket4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Socket4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socket4/Socket4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socket4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Socket4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
